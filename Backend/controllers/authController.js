e04d9ee62ba2fc87f267f570a9dd440dc9ac0f80
Digbijoy Bhattacharjee <digbijoy2003@gmail.com>
2026-02-21 04:35:34 +0600

    Created the user login and logout controller function

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -16,19 +16,70 @@ export const register = async (req, res) => {
         }
         const hashedPassword = await bycript.hash(password, 10);
 
-        const user = new userModel({name, email, password:hashedPassword});
+        const user = new userModel({ name, email, password: hashedPassword });
         await user.save();
 
-        const token = jwt.sign({id: user._id}, process.env.Secret_Key,{expiresIn:'10d'});
+        const token = jwt.sign({ id: user._id }, process.env.Secret_Key, { expiresIn: '10d' });
 
         res.cookie('token', token, {
-            httpOnly:true,
+            httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production'? 'none' : 'strict',
-            maxAge: 10*24*60*60*1000
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 10 * 24 * 60 * 60 * 1000
         });
 
+        return res.json({ success: true, message: "User Registration Successful" });
+
+
     } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.json({ success: false, message: "Email and Password is required" });
+    }
+
+    try {
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            res.json({ success: false, message: "Invalid email" });
+        }
+        const isMatch = await bycript.compare(password, user.password);
+        if (!isMatch) {
+            res.json({ success: false, message: "Invalid password" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.Secret_Key, { expiresIn: '10d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            maxAge: 10 * 24 * 60 * 60 * 1000
+        });
+
+        return res.json({ success: true, message: "User Login Successful" });
+    } catch (err) {
+        return res.status(400).json({ message: err.message });
+    }
+}
+
+export const logout = async (req, res) =>{
+    try{
+        res.clearCookie('token',{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+
+        return res.json({ success: true, message: "User Logout Successful" });
+
+
+    }catch (err) {
         return res.status(400).json({ message: err.message });
     }
 }
