2afe5feb5598b8f8c764725f6967e4d05c420e96
Digbijoy Bhattacharjee <digbijoy2003@gmail.com>
2026-02-21 02:10:15 +0600

    Created First get request in Server

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials:true}));
+
+app.get('/', (req,res)=>{
+    res.send("Backend App Started");
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is listening on Port ${3000}`); 
+});
